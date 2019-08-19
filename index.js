fd4dd09cbc69c7a635205d9a3ffd62beaeb4b01c
Rogelio <ross@thecamel.co>
2019-08-20 05:06:30 +0800

    Added New Features
    Main Categories display all the subcategories along with its products. - Done
    Sub Categories display all products of that category. - Done
    Change slider if clicking on main category or sub category and the images shown in those slider is related to the clicked category. - In Progress (Mike)
    Allow Breadcrumbs to change sub categories when clicked. - Done
    Each Main Category is an accordion when clicked will display all of its sub categories - Done
    The Filter and Products Display can both scroll just like in the craftsman products page - Done

--- index.js
+++ index.js
@@ -1,130 +1,348 @@
-// $.ajax({
-//     url: "https://spreadsheets.google.com/feeds/list/1NHLMWWftwLNjYQMgxvc2sTKc6EEH4OWfx8UGemser6s/default/public/values?alt=json",
-//     method: "GET",
-//     crossdomain: true,
-//     success: function (data) {
-//         var gsxData = data.feed.entry;
-//         //console.log(p);
-
-//         //Call all arrays
-//         let result = gsxData.reduce(function (h, obj) {
-//             h[obj.gsx$maincategory.$t] = (h[obj.gsx$maincategory.$t] || []).concat(obj);
-//             return h;
-//         }, {});
-
-//         console.log(result);
-
-//         for (var key in result) {
-//             if (result.hasOwnProperty(key)) {
-
-//                 //dynamic maincategs & subcategs
-//                 // let content = `<div class="categWrap">
-//                 // <div class="mainCateg">${key}</div>
-//                 // <div class="subCategWrapper">
-//                 // ${createCateg(result[key])}
-//                 // </div>
-//                 // <div>`;
-
-//                 // $('.mainCategWrapper').append(content);
-
-//                 //fixed maincategs & dynamic subcategs
-//                 createSubCategs(result[key])
-//             }
-//         }
-
-//         //dynamic maincategs & subcategs
-//         function createCateg(arr) {
-//             let subCategs = "";
-//             arr.map(function (i) {
-//                 subCategs += `<div class="subCateg" data-subcateg="${i.gsx$subcategory.$t}">${i.gsx$subcategory.$t}</div>`;
-//             });
-//             return subCategs;
-//         }
-
-//         //fixed maincategs & dynamic subcategs
-//         function createSubCategs(arr) {
-//             arr.map(function (i) {
-//                 let x = `<span class="wpSubCat" data-subcateg="${i.gsx$subcategory.$t}">${i.gsx$subcategoryname.$t}</span>`;
-//                 $('.wpProductSubCategory[data-maincat="' + i.gsx$maincategory.$t + '"]').append(x)
-//             });
-//         }
-
-//         //Remove Array Duuplicates
-//         function removeDuplicates(array, key) {
-//             return array.reduce(function (arr, item) {
-//                 const removed = arr.filter(function (i) {
-//                     return i[key] !== item[key];
-//                 });
-//                 return [...removed, item];
-//             }, []);
-//         };
-//     }
-// });
-
-
 //MAIN PROCESS
+let defaultSubCateg = 'FOLDUK';
+let defaultMainCateg = 'Cutting Tools';
 
-    //Get Raw GSX Data
-    const option = {
-        url:'https://spreadsheets.google.com/feeds/list/1NHLMWWftwLNjYQMgxvc2sTKc6EEH4OWfx8UGemser6s/default/public/values?alt=json',
-        method:'GET'
-    };
+//Get Raw GSX Data
+const option = {
+    url: 'https://spreadsheets.google.com/feeds/list/1NHLMWWftwLNjYQMgxvc2sTKc6EEH4OWfx8UGemser6s/default/public/values?alt=json',
+    method: 'GET'
+};
 
-    try{
-        let gsxData = getGSXData(option);
-        gsxData.then(function(data){
-            //Construct Object Data
-            console.log(constructData(data));
-        });
-        //refineGSXData(rawData);
-    }catch(e){
-        console.log(e);
-    }
+try {
+    let gsxData = getGSXData(option);
+    gsxData.then(function (data) {
+        //Construct Object Data
+        let consObject = constructData(data);
+        propList = constructData(data);
 
-    //Line 3001
-    //Factory Function for GSX Data
-    async function getGSXData(option){
-        try{
-            const result = await $.ajax(option);
-            return result.feed.entry;
-        }catch(error){
-            throw new Error('getGSXData function Failed');
+        //Group Products into Main Products
+        let groupProd = groupInto(consObject);
+
+        for (var key in groupProd) {
+            if (groupProd.hasOwnProperty(key)) {
+                //Create Subcategs
+                createSubCategs(groupProd[key])
+            }
         }
-    }
 
-    //Construct Data from GSX Data
-    function constructData(data){
-        const constructedData=[];
-        data.map(function(i){
-            constructedData.push({
-                'Item_Number': i.gsx$itemnumber.$t,
-                'Product_Name': i.gsx$productname.$t
+        //POPULATE PRODUCTS DEPENDING IF THERE IS URL PARAM OR NONE
+        let urlSubCateg = getParameterByName('prodSubcateg');
+        let urlMainCateg = getParameterByName('prodMaincateg');
+        let urlCateg = getParameterByName('prodcateg');
+        if(urlSubCateg == 'prodSubcateg'){
+            let subCat = getSubCateg(consObject, urlSubCateg);
+            createBreadcrumbs(consObject, subCat[0].main_Category) //Create Breadcrumbs
+            $('.wpProductShowcaseMainCat').text(subCat[0].main_Category);
+            $('.wpProductShowcaseSubCat').text('');
+            removeDuplicates(subCat,"sub_Category").map(function(i) {
+                let output =`<div class="wpProductsContainer" data-subcateg="${i.sub_Category}">
+                    <div class="wpProductShowcaseSubCat" ><span>${i.sub_CategoryName}</span></div></div>` ;
+                $('.wpProdContWrap').append(output);
+                return i;
+            });            
+            $('.wpProductsContainer').each(function(){
+                let thisSubCat = $(this).attr("data-subcateg");                
+                subCat.forEach(i=>{    
+                    if(i.sub_Category == thisSubCat){
+                        $('.wpProductsContainer[data-subcateg="' + thisSubCat + '"]').append(createProduct(i));
+                    }
+                });
+            });    
+        }
+        if(urlMainCateg == 'prodMaincateg'){
+            $('.wpProdContWrap').html("");
+            let mainCategory = urlMainCateg;
+            $('.wpProductCatTitle').removeClass('activeCat');
+            $(this).addClass('activeCat');
+            let subCat = getSubCateg(consObject, mainCategory);
+            createBreadcrumbs(consObject, subCat[0].main_Category); //Create Breadcrumbs
+            $('.wpProductShowcaseMainCat').text(subCat[0].main_Category);
+            $('.wpProductShowcaseSubCat').text('');
+            $('.wpProductsContainer').hide().empty();
+            removeDuplicates(subCat,"sub_Category").map(function(i) {
+                let output =`<div class="wpProductsContainer" data-subcateg="${i.sub_Category}">
+                    <div class="wpProductShowcaseSubCat" ><span>${i.sub_CategoryName}</span></div></div>` ;
+                $('.wpProdContWrap').append(output);
+                return i;
             });
+            
+            $('.wpProductsContainer').each(function(){
+                let thisSubCat = $(this).attr("data-subcateg");                
+                subCat.forEach(i=>{    
+                    if(i.sub_Category == thisSubCat){
+                        $('.wpProductsContainer[data-subcateg="' + thisSubCat + '"]').append(createProduct(i));
+                    }
+                });
+            });            
+
+            if ($('.wpProductSubCategory[data-maincat="' + mainCategory + '"]').is(':hidden')) {
+                $('.wpProductSubCategory').slideUp();
+                $('.wpProductSubCategory[data-maincat="' + mainCategory + '"]').slideToggle();
+            } else {
+                $('.wpProductSubCategory').slideUp();
+            }
+        }
+        if (urlMainCateg != null || urlMainCateg != undefined || urlSubCateg != null || urlSubCateg != undefined) {
+            let subCat = getSubCateg(consObject, defaultMainCateg);
+            createBreadcrumbs(consObject, subCat[0].main_Category) //Create Breadcrumbs
+            $('.wpProductShowcaseMainCat').text(subCat[0].main_Category);
+            $('.wpProductShowcaseSubCat').text('');
+            removeDuplicates(subCat,"sub_Category").map(function(i) {
+                let output =`<div class="wpProductsContainer" data-subcateg="${i.sub_Category}">
+                    <div class="wpProductShowcaseSubCat" ><span>${i.sub_CategoryName}</span></div></div>` ;
+                $('.wpProdContWrap').append(output);
+                return i;
+            });            
+            $('.wpProductsContainer').each(function(){
+                let thisSubCat = $(this).attr("data-subcateg");                
+                subCat.forEach(i=>{    
+                    if(i.sub_Category == thisSubCat){
+                        $('.wpProductsContainer[data-subcateg="' + thisSubCat + '"]').append(createProduct(i));
+                    }
+                });
+            }); 
+        } else {          
+            let subCat = getSubCateg(consObject, defaultMainCateg);
+            createBreadcrumbs(consObject, subCat[0].main_Category) //Create Breadcrumbs
+            $('.wpProductShowcaseMainCat').text(subCat[0].main_Category);
+            $('.wpProductShowcaseSubCat').text('');
+            removeDuplicates(subCat,"sub_Category").map(function(i) {
+                let output =`<div class="wpProductsContainer" data-subcateg="${i.sub_Category}">
+                    <div class="wpProductShowcaseSubCat" ><span>${i.sub_CategoryName}</span></div></div>` ;
+                $('.wpProdContWrap').append(output);
+                return i;
+            });            
+            $('.wpProductsContainer').each(function(){
+                let thisSubCat = $(this).attr("data-subcateg");                
+                subCat.forEach(i=>{    
+                    if(i.sub_Category == thisSubCat){
+                        $('.wpProductsContainer[data-subcateg="' + thisSubCat + '"]').append(createProduct(i));
+                    }
+                });
+            });              
+        }
+
+        //ONCLICK FILTER THROUGH SUBCATEG
+        $('.wpProductFilter').on('click', '.wpSubCat', function () {
+            let selectedCateg = $(this).data('categ');
+            $('.wpProductsContainer[data-subcateg="'+selectedCateg+'"]').show();
+            $('.wpProductsContainer:not([data-subcateg="'+selectedCateg+'"])').css({'margin-top':'0'});
+            filterProducts(consObject, selectedCateg);
         });
-        return constructedData;
+
+        //ONCLICK FILTER THROUGH BREADCRUMB
+        $('.wpProductShowcaseBreadcrumb').on('click', '.subCategBreadCrumbs', function () {
+            let selectedCateg = $(this).data('categ');
+            $('.wpProductsContainer[data-subcateg="'+selectedCateg+'"]').show();
+            $('.wpProductsContainer:not([data-subcateg="'+selectedCateg+'"])').css({'margin-top':'0'});
+            filterProducts(consObject, selectedCateg);
+        });
+
+        $('.wpProductFilter').on('click', '.wpProductCatTitle', function () {
+            $('.wpProdContWrap').html("");
+            let mainCategory = $(this).data('maincat');
+            $('.wpProductCatTitle').removeClass('activeCat');
+            $(this).addClass('activeCat');
+            let subCat = getSubCateg(consObject, mainCategory);
+            createBreadcrumbs(consObject, subCat[0].main_Category); //Create Breadcrumbs
+            $('.wpProductShowcaseMainCat').text(subCat[0].main_Category);
+            $('.wpProductShowcaseSubCat').text('');
+            $('.wpProductsContainer').hide().empty();
+            removeDuplicates(subCat,"sub_Category").map(function(i) {
+                let output =`<div class="wpProductsContainer" data-subcateg="${i.sub_Category}">
+                    <div class="wpProductShowcaseSubCat" ><span>${i.sub_CategoryName}</span></div></div>` ;
+                $('.wpProdContWrap').append(output);
+                return i;
+            });
+            
+            $('.wpProductsContainer').each(function(){
+                let thisSubCat = $(this).attr("data-subcateg");                
+                subCat.forEach(i=>{    
+                    if(i.sub_Category == thisSubCat){
+                        $('.wpProductsContainer[data-subcateg="' + thisSubCat + '"]').append(createProduct(i));
+                    }
+                });
+            });            
+
+            if ($('.wpProductSubCategory[data-maincat="' + mainCategory + '"]').is(':hidden')) {
+                $('.wpProductSubCategory').slideUp();
+                $('.wpProductSubCategory[data-maincat="' + mainCategory + '"]').slideToggle();
+            } else {
+                $('.wpProductSubCategory').slideUp();
+            }
+        });
+
+    });    
+
+    $(window).scroll(function () {
+        let winPos = $(window).scrollTop(),
+            prodShowcase = $('.wpProductShowcase').outerHeight();
+        winPos >= prodShowcase - 450 ? $('.wpProductFilter').css({
+            'position': 'relative',
+            'margin-bottom': '0',
+            'margin-top': 'auto',
+            'width': '28%'
+        }) : $('.wpProductFilter').css({
+            'position': '',
+            'margin-bottom': '',
+            'margin-top': '',
+            'width': ''
+        });
+    });
+
+} catch (e) {
+    console.log(e);
+}
+
+//Line 3001
+//Factory Function for GSX Data
+async function getGSXData(option) {
+    try {
+        const result = await $.ajax(option);
+        return result.feed.entry;
+    } catch (error) {
+        throw new Error('getGSXData function Failed');
     }
+}
+
+//Create Products
+function createProduct(prod) {
+    let item = `<div class="wpProductWrap" data-subcat="${prod.sub_Category}">
+                    <div class="innerWpProduct">
+                        <div class="wpProductImgWrap">
+                            ${prod.new_Product == 'Yes' ? '<img class="end-pro-end" src="http://www.workprotools.com/static/web/img/img-56.png?v=v1 " alt="">': ""}
+                            <img src="${prod.image}" alt="">
+                        </div>
+                        <div class="wpProductInfoWrap">
+                            <div class="wpProductName">${prod.product_Name}</div>
+                            <div class="wpProductItemNum">ITEM# : ${prod.item_Number}</div>
+                            <div class="wpProductReviews"></div>
+                        </div>
+                    </div>
+                </div>`
+    return item;
+}
+
+//Filter Array based on Category
+function getProds(arr, categ) {
+    let filteredProd = arr.filter(function (obj) {
+        return obj.sub_Category === categ;
+    });
+    return filteredProd;
+}
+
+function getSubCateg(arr, maincateg){
+    let filteredCat = arr.filter(function(obj){
+        return obj.main_Category === maincateg;
+    });
+    return filteredCat;
+}
+
+//Filter Products based on SubCategories
+function filterProducts(consObject, selectedCateg) {    
+    $('.wpProductsContainer').html('');
+    let fProds = getProds(consObject, selectedCateg); //FILTERED PRODUCTS BASED ON BUTTON CATEG
+    createBreadcrumbs(consObject, fProds[0].main_Category) //Create Breadcrumbs
+    $('.wpProductShowcaseMainCat').text(fProds[0].main_Category);
+    $('.wpProductShowcaseSubCat').text(fProds[0].sub_CategoryName);
+    $('.wpProductsContainer').hide().empty();
+    fProds.map(function (i) {
+        $('.wpProductsContainer[data-subcateg='+selectedCateg+']').append(createProduct(i));
+    });
+    $('.wpProductsContainer').fadeIn();
+}
+
+//Group Products into Main Products
+function groupInto(consObject) {
+    let result = consObject.reduce(function (h, obj) {
+        h[obj.main_Category] = (h[obj.main_Category] || []).concat(obj);
+        return h;
+    }, {});
+    return result;
+}
+
+//Crete and Append Subcategories
+function createSubCategs(data) {
+    let result = data.reduce(function (memo, e1) {
+        var matches = memo.filter(function (e2) {
+            return e1.sub_Category == e2.sub_Category
+        });
+        if (matches.length == 0)
+            memo.push(e1)
+        return memo;
+    }, []);
+
+    result.map(function (i) {
+        let subCateg = `<div class="wpSubCat" data-categ="${i.sub_Category}">${i.sub_CategoryName}</div>`;
+        $('.wpProductSubCategory[data-maincat="' + i.main_Category + '"]').append(subCateg);
+    });
+
+}
+
+//Create Breadcrumbs
+function createBreadcrumbs(arr, mainCateg) {
+    let filteredProd = arr.filter(function (obj) {
+        return obj.main_Category === mainCateg;
+    });
+
+    let filterdSubCateg = filteredProd.reduce(function (memo, e1) {
+        var matches = memo.filter(function (e2) {
+            return e1.sub_Category == e2.sub_Category
+        })
+        if (matches.length == 0)
+            memo.push(e1)
+        return memo;
+    }, []);
 
 
+    let output = '';
+    filterdSubCateg.map(a => {
+        output += `<span class="subCategBreadCrumbs" data-categ="${a.sub_Category}">${a.sub_CategoryName}</span> / `
+    });
+    $('.wpProductShowcaseBreadcrumb').html(output.slice(0, -2));
+}
 
+//Construct Data from GSX Data
+function constructData(data) {
+    const constructedData = [];
+    data.map(function (i) {
+        constructedData.push({
+            'item_Number': i.gsx$itemnumber.$t,
+            'product_Name': i.gsx$productname.$t,
+            'new_Product': i.gsx$newproduct.$t,
+            'main_Category': i.gsx$maincategory.$t,
+            'sub_Category': i.gsx$subcategory.$t,
+            'sub_CategoryName': i.gsx$subcategoryname.$t,
+            'rating': i.gsx$rating.$t,
+            'link': i.gsx$link.$t,
+            'image': i.gsx$image.$t
+        });
+    });
+    return constructedData;
+}
 
+//Get URL parameters value
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
 
+//Remove Duplicate Objects
+function removeDuplicates(array, key){
+    return array.reduce(function(arr, item) {
+       const removed = arr.filter(function(i){
+           return i[key] !== item[key];
+       });
+       return [...removed, item];
+   },[]);
+};
 
-
-
-
-
-$('.wpSubCat').on('click', function () {
-    $('.wpSubCat').removeClass('activeCat');
-    $(this).addClass('activeCat');
-});
-
-$(".wpSubCat").on({
-    mouseenter: function () {
-        $(this).addClass('hoverCat');
-    },
-    mouseleave: function () {
-        $(this).removeClass('hoverCat');
-    }
-});
-
-
+// let result = gsxData.reduce(function (h, obj) {
+//     h[obj.gsx$maincategory.$t] = (h[obj.gsx$maincategory.$t] || []).concat(obj);
+//     return h;
+// }, {});
